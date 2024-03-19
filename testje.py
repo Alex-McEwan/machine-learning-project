0583c3136eaafa8a30dc6048e563d33b6ffcc6f5
alex.mcewan <58659503+Baalekker@users.noreply.github.com>
2024-03-19 15:24:55 +0100

    tensorflow testn

--- testje.py
+++ testje.py
@@ -1,2 +1,8 @@
-import numpy as np
+import tensorflow as tf
 
+# Check TensorFlow version
+print("TensorFlow version:", tf.__version__)
+
+# List available GPUs
+gpus = tf.config.list_physical_devices('GPU')
+print(gpus)
